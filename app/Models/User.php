973f22d6751bed6e07a127a4eadd48df57f1eb2c
healerv2 <30591396+healerv2@users.noreply.github.com>
2021-08-24 15:53:42 +0700

    commit asset

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
+        'phone',
+        'address',
         'email',
+        'level',
         'password',
     ];
 
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+       $user = [
+        [
+            'username' => 'superadmin',
+            'name'=>'superadmin',
+            'email'=>'admin@example.com',
+            'phone'=>'0',
+            'address'=>'mlg',
+            'level'=>'superadmin',
+            'password'=> bcrypt('superadmin'),
+        ],
+        [
+            'username' => 'admin accounting',
+            'name'=>'accounting',
+            'email'=>'payment@example.com',
+            'phone'=>'0',
+            'address'=>'mlg',
+            'level'=>'accounting',
+            'password'=> bcrypt('123'),
+        ],
+        [
+            'username' => 'user',
+            'name'=>'user',
+            'email'=>'user@example.com',
+            'phone'=>'0',
+            'address'=>'mlg',
+            'level'=>'visitor',
+            'password'=> bcrypt('123456'),
+        ],
+    ];
+
+    foreach ($user as $key => $value) {
+        User::create($value);
+    }
+}
+}
