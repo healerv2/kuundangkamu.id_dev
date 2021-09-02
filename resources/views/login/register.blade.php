a6d1d2c5988e1f6a6bf4b21d783211e45fffd11e
healerv2 <30591396+healerv2@users.noreply.github.com>
2021-09-02 13:58:04 +0700

    cek otp not fix

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -15,59 +15,62 @@
   <link rel="stylesheet" href="{{url('')}}/assets/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition register-page">
-<div class="register-box">
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Register a new membership</p>
+  <div class="register-box">
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">Register a new membership</p>
 
-      <form action="../../index.html" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+        <form action="../../index.html" method="post">
+                {{ csrf_field() }}
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Full name">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Phone (WA)">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-mobile"></span>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Phone (WA)">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-mobile"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" name="otp" id="otp" placeholder="Otp">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <a href="{{ url('home/send/otp') }}"class="btn-primary btn-sm">Send Otp</a>
+                {{-- <span class="fas fa-lock"></span> --}}
+              </div>
+              <span id="error_otp"></span>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-              <label for="agreeTerms">
-               I agree to the <a href="#">terms</a>
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          <div class="row">
+            <div class="col-8">
+              <div class="icheck-primary">
+                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                <label for="agreeTerms">
+                 I agree to the <a href="#">terms</a>
+               </label>
+             </div>
+           </div>
+           <!-- /.col -->
+           <div class="col-4">
+            <button type="submit" name="register" id="register" class="btn btn-primary btn-block">Register</button>
           </div>
           <!-- /.col -->
         </div>
@@ -93,5 +96,46 @@
 <script src="{{url('')}}/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="{{url('')}}/assets/dist/js/adminlte.min.js"></script>
+<script>
+  $(document).ready(function(){
+
+   $('#otp').blur(function(){
+    var error_otp = '';
+    var otp = $('#otp').val();
+    var _token = $('input[name="_token"]').val();
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(!filter.test(otp))
+    {    
+     $('#error_otp').html('<label class="text-danger">Otp tidak sesuai, mohon dicek!</label>');
+     $('#otp').addClass('has-error');
+     $('#register').attr('disabled', 'disabled');
+   }
+   else
+   {
+     $.ajax({
+     url: '{{ url('/')}}/otp/check',
+      method:"POST",
+      data:{otp:otp, _token:_token},
+      success:function(result)
+      {
+       //if(result == 'unique')
+       {
+        $('#error_otp').html('<label class="text-success">Otp Available</label>');
+        $('#otp').removeClass('has-error');
+        $('#register').attr('disabled', false);
+      }
+      else
+      {
+        $('#error_otp').html('<label class="text-danger">Otp not Available</label>');
+        $('#otp').addClass('has-error');
+        $('#register').attr('disabled', 'disabled');
+      }
+    }
+  })
+   }
+ });
+
+ });
+</script>
 </body>
 </html>
