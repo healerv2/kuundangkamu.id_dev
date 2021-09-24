dbc9202defc7df87f9b5f6ec51328a15a91bf12c
bgpnd <ihamunderscoreirw@gmail.com>
2021-09-25 05:45:37 +0700

    fixing template

--- resources/views/visitor/apps/template.blade.php
+++ resources/views/visitor/apps/template.blade.php
@@ -12,60 +12,20 @@
             <button data-filter=".metal:not(.transition)">metal but not transition</button>
         </div>
         
+        {{--  @foreach ($template->chunk(3) as $chunk) --}}
         <ul class="grid -unstyled">
-            <li class="grid-item basic">
-                <figure style="background-image: url(/image/20210917133037.png)">
-                    <span>Basic</span>
-                </figure>
-                <p>Template 1</p>
-                <a href="#" target="_blank" class="btn btn--white">Demo</a>
-                <a href="#t" class="btn">Pilih</a>
-            </li>
-            <li class="grid-item premium">
-                <figure style="background-image: url(/image/20210917133037.png)">
-                    <span>Premium</span>
-                </figure>
-                <p>Template 1</p>
-                <a href="#" target="_blank" class="btn btn--white">Demo</a>
-                <a href="#t" class="btn">Pilih</a>
-            </li>
-            <li class="grid-item eksklusif">
-                <figure style="background-image: url(/image/20210917133037.png)">
-                    <span>Eksklusif</span>
-                </figure>
-                <p>Template 1</p>
-                <a href="#" target="_blank" class="btn btn--white">Demo</a>
-                <a href="#t" class="btn">Pilih</a>
-            </li>
-            <li class="grid-item premium">
-                <figure style="background-image: url(/image/20210917133037.png)">
-                    <span>Premium</span>
-                </figure>
-                <p>Template 1</p>
-                <a href="#" target="_blank" class="btn btn--white">Demo</a>
-                <a href="#t" class="btn">Pilih</a>
-            </li>
-            <li class="grid-item basic">
-                <figure style="background-image: url(/image/20210917133037.png)">
-                    <span>Basic</span>
-                </figure>
-                <p>Template 1</p>
-                <a href="#" target="_blank" class="btn btn--white">Demo</a>
-                <a href="#t" class="btn">Pilih</a>
-            </li>
-        </ul>
-        <!-- {{--  @foreach ($template->chunk(3) as $chunk) --}}
-        <ul class="grid -unstyled -flex -justify-center">
             @foreach ($template as $template)
-            <li class="grid-item">
-                <figure style="background-image: url(/image/{{ $template->image }})"></figure>
+            <li class="grid-item @if(\Request::is('visitor/public') ) basic  @endif">
+                <figure style="background-image: url(/image/{{ $template->image }})">
+                    <span>Basic</span>
+                </figure>
                 <p>{{ $template->name_template}}</p>
                 <a href="{{ $template->url}}" target="_blank" class="btn btn--white">Demo</a>
                 <a href="{{url('')}}/visitor/checkout" class="btn">Pilih</a>
             </li>
             @endforeach
         </ul>
-        {{--  @endforeach --}} -->
+        {{--  @endforeach --}}
     </div>
 </div>
 
